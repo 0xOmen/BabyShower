4e1b4bc8ef988fb709b9cd9814c2a493984e1433
0xomen <ox0men21@gmail.com>
2025-09-01 14:28:24 -0600

    Change loading screen color

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -66,7 +66,7 @@ export const APP_SPLASH_URL: string = `${APP_URL}/icon.png`;
  * Background color for the splash screen.
  * Used as fallback when splash image is loading.
  */
-export const APP_SPLASH_BACKGROUND_COLOR: string = "#f7f7f7";
+export const APP_SPLASH_BACKGROUND_COLOR: string = "#f96da7";
 
 /**
  * Account association for the mini app.
